363af83d3ca72a414adf4ecc056b3feaa110ed79
Patrick Roach <patrickevanroach@gmail.com>
2019-01-21 16:48:04 -0700

    first try

--- assessment.js
+++ assessment.js
@@ -1,31 +1,33 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
+var myName = "Patrick Roach";
 
-// CODE HERE
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
-
-// CODE HERE
+var faveNum = 7;
 
 // #3 create a variable called 'lovesCode' and set it to true
-
+var lovesCode = true;
 // CODE HERE
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
-
+var newVar = myName + faveNum;
 // CODE HERE
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
-
+var trueFaveNum = 7;
+trueFaveNum = faveNum
 // CODE HERE
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
-
+var notHakeem = "Hakeem Olajuwon"
 // CODE HERE
-
+myName = notHakeem
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
-
+if (canDrive >= 16){
+ return true
+} else {return false};
 
 // CODE HERE
 
@@ -34,13 +36,26 @@ var canDrive;
 var colorResponse;
 
 var responseCreator = function(param){
+  colorResponse = "what is your favorite color"
+  if (param = "green"){
+    var colorResponse = "green is okay"
+  } else if (param = "red") {
+ var colorResponse = "I heart red"
+  }
+  return colorResponse
   // CODE HERE
 }
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
-
+function sum(addedToSelf){
+  addedToSelf + addedToSelf
+  return addedToSelf
+}
 // CODE HERE
 
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
+function greeter(aName,aNum){
+  return 'Hi! I am ' + aName + ' and I am ' + aNum + " years old"  
 
+}
 // CODE HERE
